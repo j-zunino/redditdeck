6c64060bf623537c94f333a99433f5fd90866583
j-zunino <zuninojuanbautista@gmail.com>
2025-02-04 20:17:33 -0300

    feat: add sidebar

--- src/components/Deck/Deck.tsx
+++ src/components/Deck/Deck.tsx
@@ -1,9 +1,12 @@
+import { Sidebar } from '../Sidebar';
 import { Subreddit } from '../Subreddit/Subreddit';
 
 export const Deck = () => {
     return (
-        <>
+        <div className="flex">
+            <Sidebar />
             <Subreddit />
-        </>
+            <Subreddit />
+        </div>
     );
 };
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,23 @@
+export const Sidebar = () => {
+    return (
+        <aside className="bg-white border-gray-200 border-r-1 flex justify-center items-center z-10 sticky left-0 top-0 p-2 h-screen text-2xl font-bold">
+            <div className="text-black p-2 hover:bg-gray-50 hover:rounded-full">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M12 5l0 14" />
+                    <path d="M5 12l14 0" />
+                </svg>
+            </div>
+        </aside>
+    );
+};
