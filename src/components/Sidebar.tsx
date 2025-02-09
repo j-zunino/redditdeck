d60af72ac19b940027d46bc06d99d937cdc6c033
j-zunino <zuninojuanbautista@gmail.com>
2025-02-09 19:19:35 -0300

    fix: firefox sidebar not aligned to start

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export const Sidebar = ({ parentMethod }: Props) => {
     return (
-        <nav className="border-r-1 sticky left-0 top-0 z-10 flex h-screen justify-center border-gray-200 bg-white p-2 text-2xl font-bold">
+        <nav className="border-r-1 sticky left-0 top-0 z-10 flex h-screen items-start justify-center border-gray-200 bg-white p-2 text-2xl font-bold">
             <button
                 onClick={parentMethod}
                 className="max-h-fit p-2 text-black hover:rounded-full hover:bg-gray-50"
