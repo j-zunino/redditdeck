ef61b981043256ddc789bac63506b83fe94b3c4b
j-zunino <zuninojuanbautista@gmail.com>
2025-02-09 15:59:14 -0300

    feat: move score before flair

--- src/components/Subreddit/Post.tsx
+++ src/components/Subreddit/Post.tsx
@@ -2,7 +2,7 @@ import { Thumbnail, UpvoteIcon, DownvoteIcon } from '../../components';
 
 interface Props {
     author: string;
-    title?: string;
+    title: string;
     score: number;
     link_flair_text: string;
     link_flair_background_color: string;
@@ -23,25 +23,42 @@ export const Post = ({
 }: Props) => {
     return (
         <>
-            <article className="m-2 flex cursor-pointer items-center rounded-xl bg-gray-50 p-4 hover:bg-gray-100">
-                <div className="m-4 ml-0 flex-row justify-center justify-items-center text-center">
-                    {score >= 0 ? <UpvoteIcon /> : <DownvoteIcon />}
-                    <p>{score}</p>
-                </div>
-                <Thumbnail thumbnail={thumbnail} is_video={is_video} />
+            <article className="m-2 flex cursor-pointer items-center rounded-xl bg-gray-50 p-2 hover:bg-gray-100">
+                <Thumbnail
+                    thumbnail={thumbnail}
+                    is_video={is_video}
+                    alt={title}
+                />
                 <div>
-                    <p>u/{author}</p>
-                    <h2 className="mb-3 font-black">{title}</h2>
-                    <span
-                        className="text-dark rounded-full p-2 py-0.5 text-xs"
-                        style={{ backgroundColor: link_flair_background_color }}
-                    >
-                        {link_flair_text}
-                    </span>
+                    <p className="md:text-md text-xs sm:text-sm">u/{author}</p>
+                    <h2 className="md:text-md mb-2 text-xs font-black sm:text-sm">
+                        {title}
+                    </h2>
+                    <div className="flex items-center text-center">
+                        <div className="mr-4 flex h-fit">
+                            {score >= 0 ? <UpvoteIcon /> : <DownvoteIcon />}
+                            <p className="flex justify-center">{score}</p>
+                        </div>
+
+                        {link_flair_text !== null ? (
+                            <span
+                                className="text-dark rounded-full p-2 py-0.5 text-xs"
+                                style={{
+                                    backgroundColor:
+                                        link_flair_background_color === '' ||
+                                        link_flair_background_color === null
+                                            ? '#E7E7E7'
+                                            : link_flair_background_color
+                                }}
+                            >
+                                {link_flair_text}
+                            </span>
+                        ) : undefined}
+                    </div>
                 </div>
             </article>
 
-            <div className="border-1 mx-2 border-gray-200"></div>
+            <div className="border-b-1 mx-2 border-gray-200"></div>
         </>
     );
 };
--- src/components/Subreddit/SubredditSkeleton.tsx
+++ src/components/Subreddit/SubredditSkeleton.tsx
@@ -1,112 +1,40 @@
 export const SubredditSkeleton = () => {
     return (
         <section className="flex-1 animate-pulse">
+            {/* Header */}
             <div className="sticky top-0 flex h-12 w-full items-center justify-center bg-gray-50 text-2xl font-bold">
                 <div className="w-50 h-7 rounded-md bg-gray-200"></div>
             </div>
-            <div className="flex flex-col items-center justify-center">
-                <div className="m-4 mb-0 flex w-[95%] items-center rounded-md bg-gray-100 p-0">
-                    <div className="m-4 flex flex-col">
-                        <div className="mb-2 rounded-full bg-gray-200 p-4"></div>
-                        <div className="rounded-md bg-gray-200 p-3"></div>
-                    </div>
-                    <div className="m-4 ml-0 h-20 w-24 rounded-md bg-gray-200 p-4"></div>
-                    <div className="flex flex-col">
-                        <div className="mb-2 w-24 rounded-full bg-gray-200 p-2"></div>
-                        <div className="w-50 mb-4 rounded-full bg-gray-200 p-2"></div>
-                        <div className="w-24 rounded-full bg-gray-200 p-3"></div>
-                    </div>
-                </div>
 
-                <div className="m-4 mb-0 flex w-[95%] items-center rounded-md bg-gray-100 p-0">
-                    <div className="m-4 flex flex-col">
-                        <div className="mb-2 rounded-full bg-gray-200 p-4"></div>
-                        <div className="rounded-md bg-gray-200 p-3"></div>
-                    </div>
-                    <div className="m-4 ml-0 h-20 w-24 rounded-md bg-gray-200 p-4"></div>
-                    <div className="flex flex-col">
-                        <div className="mb-2 w-12 rounded-full bg-gray-200 p-2"></div>
-                        <div className="w-62 mb-4 rounded-full bg-gray-200 p-2"></div>
-                        <div className="w-30 rounded-full bg-gray-200 p-3"></div>
-                    </div>
-                </div>
+            {Array.from({ length: 16 }, (_, index) => (
+                // bg
+                <section
+                    key={index}
+                    className="h-25 m-4 mx-2 flex items-center rounded-xl bg-gray-100 p-2"
+                >
+                    {/* Thumbnail */}
+                    <div className="min-w-24 mr-4 hidden h-20 w-24 rounded-md bg-gray-200 lg:flex"></div>
 
-                <div className="m-4 mb-0 flex w-[95%] items-center rounded-md bg-gray-100 p-0">
-                    <div className="m-4 flex flex-col">
-                        <div className="mb-2 rounded-full bg-gray-200 p-4"></div>
-                        <div className="rounded-md bg-gray-200 p-3"></div>
-                    </div>
-                    <div className="m-4 ml-0 h-20 w-24 rounded-md bg-gray-200 p-4"></div>
-                    <div className="flex flex-col">
-                        <div className="mb-1 w-28 rounded-full bg-gray-200 p-2"></div>
-                        <div className="flex">
-                            <div className="mb-1 mr-4 w-20 rounded-full bg-gray-200 p-2"></div>
-                            <div className="w-62 mb-1 rounded-full bg-gray-200 p-2"></div>
+                    <div>
+                        {/* author */}
+                        <div className="mb-2 h-4 w-20 rounded-md bg-gray-200"></div>
+
+                        {/* title */}
+                        <div className="w-70 mb-4 h-4 rounded-md bg-gray-200"></div>
+
+                        <div className="flex items-center text-center">
+                            {/* score */}
+                            <div className="mr-4 flex">
+                                <div className="mr-1 h-4 w-4 rounded-full bg-gray-200"></div>
+                                <div className="h-4 w-10 rounded-md bg-gray-200"></div>
+                            </div>
+
+                            {/* flair */}
+                            <div className="h-4 w-20 rounded-full bg-gray-200"></div>
                         </div>
-                        <div className="w-62 mb-2 rounded-full bg-gray-200 p-2"></div>
-                        <div className="w-44 rounded-full bg-gray-200 p-3"></div>
                     </div>
-                </div>
-
-                <div className="m-4 mb-0 flex w-[95%] items-center rounded-md bg-gray-100 p-0">
-                    <div className="m-4 flex flex-col">
-                        <div className="mb-2 rounded-full bg-gray-200 p-4"></div>
-                        <div className="rounded-md bg-gray-200 p-3"></div>
-                    </div>
-                    <div className="m-4 ml-0 h-20 w-24 rounded-md bg-gray-200 p-4"></div>
-                    <div className="flex flex-col">
-                        <div className="mb-2 w-28 rounded-full bg-gray-200 p-2"></div>
-                        <div className="flex">
-                            <div className="mb-4 mr-4 w-20 rounded-full bg-gray-200 p-2"></div>
-                            <div className="w-62 mb-4 rounded-full bg-gray-200 p-2"></div>
-                        </div>
-                        <div className="w-16 rounded-full bg-gray-200 p-3"></div>
-                    </div>
-                </div>
-
-                <div className="m-4 mb-0 flex w-[95%] items-center rounded-md bg-gray-100 p-0">
-                    <div className="m-4 flex flex-col">
-                        <div className="mb-2 rounded-full bg-gray-200 p-4"></div>
-                        <div className="rounded-md bg-gray-200 p-3"></div>
-                    </div>
-                    <div className="m-4 ml-0 h-20 w-24 rounded-md bg-gray-200 p-4"></div>
-                    <div className="flex flex-col">
-                        <div className="mb-2 w-12 rounded-full bg-gray-200 p-2"></div>
-                        <div className="w-62 mb-4 rounded-full bg-gray-200 p-2"></div>
-                        <div className="w-30 rounded-full bg-gray-200 p-3"></div>
-                    </div>
-                </div>
-
-                <div className="m-4 mb-0 flex w-[95%] items-center rounded-md bg-gray-100 p-0">
-                    <div className="m-4 flex flex-col">
-                        <div className="mb-2 rounded-full bg-gray-200 p-4"></div>
-                        <div className="rounded-md bg-gray-200 p-3"></div>
-                    </div>
-                    <div className="m-4 ml-0 h-20 w-24 rounded-md bg-gray-200 p-4"></div>
-                    <div className="flex flex-col">
-                        <div className="mb-2 w-24 rounded-full bg-gray-200 p-2"></div>
-                        <div className="w-50 mb-4 rounded-full bg-gray-200 p-2"></div>
-                        <div className="w-24 rounded-full bg-gray-200 p-3"></div>
-                    </div>
-                </div>
-
-                <div className="m-4 mb-0 flex w-[95%] items-center rounded-md bg-gray-100 p-0">
-                    <div className="m-4 flex flex-col">
-                        <div className="mb-2 rounded-full bg-gray-200 p-4"></div>
-                        <div className="rounded-md bg-gray-200 p-3"></div>
-                    </div>
-                    <div className="m-4 ml-0 h-20 w-24 rounded-md bg-gray-200 p-4"></div>
-                    <div className="flex flex-col">
-                        <div className="mb-1 w-28 rounded-full bg-gray-200 p-2"></div>
-                        <div className="flex">
-                            <div className="mb-1 mr-4 w-20 rounded-full bg-gray-200 p-2"></div>
-                            <div className="w-62 mb-1 rounded-full bg-gray-200 p-2"></div>
-                        </div>
-                        <div className="w-62 mb-2 rounded-full bg-gray-200 p-2"></div>
-                        <div className="w-44 rounded-full bg-gray-200 p-3"></div>
-                    </div>
-                </div>
-            </div>
+                </section>
+            ))}
         </section>
     );
 };
