26e793068b11e5bf59f445b1443f8b2606e5d235
j-zunino <zuninojuanbautista@gmail.com>
2025-02-10 03:01:05 -0300

    feat(sidebar): add github link

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { IconPlus } from '@tabler/icons-react';
+import { IconBrandGithub, IconPlus } from '@tabler/icons-react';
 
 interface Props {
     parentMethod?: () => void;
@@ -6,7 +6,7 @@ interface Props {
 
 export const Sidebar = ({ parentMethod }: Props) => {
     return (
-        <nav className="border-r-1 sticky left-0 top-0 z-10 flex h-screen items-start justify-center border-gray-200 bg-white p-2 text-2xl font-bold">
+        <nav className="border-r-1 sticky left-0 top-0 z-10 flex h-screen flex-col items-start justify-between border-gray-200 bg-white p-2 text-2xl font-bold">
             <button
                 onClick={parentMethod}
                 role="button"
@@ -15,6 +15,19 @@ export const Sidebar = ({ parentMethod }: Props) => {
             >
                 <IconPlus />
             </button>
+            <a
+                aria-label="Add Subreddit"
+                className="max-h-fit p-2 text-black hover:rounded-full hover:bg-gray-50"
+            ></a>
+            <a
+                href="https://github.com/j-zunino/redditdeck"
+                target="_blank"
+                aria-label="See source code in GitHub"
+                rel="noreferrer"
+                className="max-h-fit p-2 text-black hover:rounded-full hover:bg-gray-50"
+            >
+                <IconBrandGithub />
+            </a>
         </nav>
     );
 };
