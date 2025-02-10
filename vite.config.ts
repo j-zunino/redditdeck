711b8c0645eca746329372ab1ccde8968f0725af
j-zunino <zuninojuanbautista@gmail.com>
2025-02-10 03:06:44 -0300

    fix: browser crashing because dynamic imports
    -
    https://github.com/tabler/tabler-icons/issues/1233#issuecomment-2428245119

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,11 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()]
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            '@tabler/icons-react':
+                '@tabler/icons-react/dist/esm/icons/index.mjs'
+        }
+    }
 });
