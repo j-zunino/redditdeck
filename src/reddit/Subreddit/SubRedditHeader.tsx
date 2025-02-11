02a80b7fdab85d737ca9ed52cd64d6280c44f39a
j-zunino <zuninojuanbautista@gmail.com>
2025-02-11 20:53:39 -0300

    feat(SubRedditHeader): text size breakpoints

--- src/reddit/Subreddit/SubRedditHeader.tsx
+++ src/reddit/Subreddit/SubRedditHeader.tsx
@@ -6,9 +6,9 @@ interface Props {
 
 export const SubRedditHeader = ({ subreddit }: Props) => {
     return (
-        <div className="border-b-1 sticky top-0 flex w-full items-center justify-between border-gray-200 bg-white text-2xl font-bold">
+        <div className="border-b-1 text-md sticky top-0 flex w-full items-center justify-between border-gray-200 bg-white font-bold md:text-lg lg:text-xl">
             <h2 className="p-2">{subreddit}</h2>
-            <div className="mr-2 p-2 text-black hover:rounded-full hover:bg-gray-50">
+            <div className="m-1 p-2 text-black hover:rounded-full hover:bg-gray-50">
                 <IconDotsVertical />
             </div>
         </div>
