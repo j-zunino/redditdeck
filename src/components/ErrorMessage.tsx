381804e200ebe3731643dd95512f76d031786811
j-zunino <zuninojuanbautista@gmail.com>
2025-02-24 23:41:38 -0300

    feat(ErrorMessage.tsx): update icon style

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,4 +1,4 @@
-import { IconExclamationCircle } from '@tabler/icons-react';
+import { IconExclamationCircleFilled } from '@tabler/icons-react';
 
 interface Props {
     errorMessage: string;
@@ -6,9 +6,16 @@ interface Props {
 
 export const ErrorMessage = ({ errorMessage }: Props) => {
     return (
-        <div className="flex h-screen flex-1 flex-col items-center justify-center text-center text-red-500 dark:bg-black">
-            <div className="animate-pulse">
-                <IconExclamationCircle />
+        <div className="flex h-screen flex-1 flex-col items-center justify-center text-center text-red-500">
+            <div className="mb-2 inline-flex">
+                <IconExclamationCircleFilled
+                    size={34}
+                    className="absolute animate-ping"
+                />
+                <IconExclamationCircleFilled
+                    size={34}
+                    className="animate-pulse"
+                />
             </div>
             <h2 className="text-xl font-bold">UPS! There has ben an error:</h2>
             <p>{errorMessage}</p> );
