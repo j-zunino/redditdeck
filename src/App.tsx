55b8e6ee0765449016da1350ddd5944d090d2b21
j-zunino <zuninojuanbautista@gmail.com>
2025-02-26 00:44:27 -0300

    feat(Modal.tsx): make modal more modular

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import { useState } from 'react';
-import { Deck, Modal, Sidebar } from './components';
+import { useEffect, useState } from 'react';
+import { ButtonIcon, Deck, Modal, Sidebar } from './components';
+import {
+    IconArrowLeft,
+    IconCancel,
+    IconExclamationCircleFilled,
+    IconPlus
+} from '@tabler/icons-react';
 
 function App() {
     const [subRedditList, setSubRedditList] = useState(['all']);
+    const [inputValue, setInputValue] = useState('');
     const [isOpen, setIsOpen] = useState(false);
+
+    useEffect(() => {
+        if (!isOpen) {
+            setInputValue('');
+        }
+    }, [isOpen]);
 
     const handleAddSubreddit = (subreddit: string) => {
         if (subreddit.trim() && !subRedditList.includes(subreddit.trim())) {
@@ -12,18 +25,51 @@ function App() {
         setIsOpen(false);
     };
 
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        if (inputValue.trim()) {
+            handleAddSubreddit(inputValue);
+        }
+    };
+
     return (
         <>
-            <Modal
-                title="Enter Name"
-                body="Enter the name of the subreddit to add."
-                placeHolder="Neovim"
-                button="Cancel"
-                buttonAccept="Add"
-                isOpen={isOpen}
-                onSubmit={handleAddSubreddit}
-                onClose={() => setIsOpen(false)}
-            />
+            <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+                <div className="mb-4 text-center">
+                    <h2 className="text-xl font-bold">Enter name</h2>
+                    <p className="text-gray-600 dark:text-zinc-400">
+                        Enter the name of the subreddit to add.
+                    </p>
+                </div>
+                <form onSubmit={handleSubmit} className="flex flex-col">
+                    <input
+                        required
+                        name="subRedditInput"
+                        autoFocus={true}
+                        value={inputValue}
+                        type="text"
+                        onChange={(event) => setInputValue(event.target.value)}
+                        placeholder="Neovim"
+                        className="mx-10 mb-4 rounded-full bg-gray-100 p-2 px-4 dark:bg-zinc-700"
+                    />
+                    <div className="flex justify-center">
+                        <ButtonIcon
+                            icon={<IconCancel size={20} />}
+                            labelEnd="Cancel"
+                            ariaLabel="Cancel"
+                            onClick={() => setIsOpen(false)}
+                            className="mr-2 bg-gray-100 pr-4 text-gray-500 hover:bg-gray-200 dark:bg-zinc-700 dark:hover:bg-zinc-800"
+                        />
+                        <ButtonIcon
+                            icon={<IconPlus size={20} />}
+                            type="submit"
+                            labelEnd="Add"
+                            ariaLabel="Add SubReddit"
+                            className="bg-orange-600 pr-4 hover:bg-orange-700"
+                        />
+                    </div>
+                </form>
+            </Modal>
 
             <div className="flex h-screen dark:bg-black dark:text-white">
                 <Sidebar parentMethod={() => setIsOpen(true)} />
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,45 +1,14 @@
-import { useEffect, useState } from 'react';
+import { ReactNode } from 'react';
 import { useClickOutside } from '../hooks';
-import { ButtonIcon } from './ButtonIcon';
-import { IconCancel, IconPlus } from '@tabler/icons-react';
 
 interface Props {
-    title: string;
-    body: string;
-    placeHolder?: string;
-    button?: string;
-    buttonAccept: string;
+    children: ReactNode;
     isOpen: boolean;
     onClose: () => void;
-    onSubmit?: (input: string) => void | undefined;
 }
 
-export const Modal = ({
-    title,
-    body,
-    placeHolder,
-    button,
-    buttonAccept,
-    isOpen,
-    onClose,
-    onSubmit
-}: Props) => {
-    const [inputValue, setInputValue] = useState('');
+export const Modal = ({ children, isOpen, onClose }: Props) => {
     const { elementRef } = useClickOutside(onClose);
-
-    useEffect(() => {
-        if (!isOpen) {
-            setInputValue('');
-        }
-    }, [isOpen]);
-
-    const handleSubmit = (event: React.FormEvent) => {
-        event.preventDefault();
-        if (!onSubmit) return;
-        if (inputValue.trim()) {
-            onSubmit(inputValue);
-        }
-    };
 
     if (!isOpen) return null;
 
@@ -50,44 +19,7 @@ export const Modal = ({
                 role="dialog"
                 className="border-1 fixed inset-x-0 top-20 z-50 mx-auto flex max-h-fit max-w-xl flex-col rounded-md border-gray-200 bg-white p-12 px-4 text-black dark:border-zinc-800 dark:bg-zinc-900 dark:text-white sm:w-3/4"
             >
-                <div className="mb-4 text-center">
-                    <h2 className="text-xl font-bold">{title}</h2>
-                    <p className="text-gray-600 dark:text-zinc-400">{body}</p>
-                </div>
-                <form onSubmit={handleSubmit} className="flex flex-col">
-                    {placeHolder ? (
-                        <input
-                            required
-                            name="subRedditInput"
-                            autoFocus={true}
-                            value={inputValue}
-                            type="text"
-                            onChange={(event) =>
-                                setInputValue(event.target.value)
-                            }
-                            placeholder={placeHolder}
-                            className="mx-10 mb-4 rounded-full bg-gray-100 p-2 px-4 dark:bg-zinc-700"
-                        />
-                    ) : null}
-                    <div className="flex justify-center">
-                        {button ? (
-                            <ButtonIcon
-                                icon={<IconCancel size={20} />}
-                                labelEnd={button}
-                                ariaLabel={button}
-                                onClick={onClose}
-                                className="mr-2 bg-gray-100 pr-4 text-gray-500 hover:bg-gray-200 dark:bg-zinc-700 dark:hover:bg-zinc-800"
-                            />
-                        ) : null}
-                        <ButtonIcon
-                            icon={<IconPlus size={20} />}
-                            labelEnd={buttonAccept}
-                            ariaLabel={buttonAccept}
-                            type="submit"
-                            className="bg-orange-600 pr-4 hover:bg-orange-700"
-                        />
-                    </div>
-                </form>
+                {children}
             </article>
 
             <div className="absolute z-20 h-full w-full bg-black/20"></div>
