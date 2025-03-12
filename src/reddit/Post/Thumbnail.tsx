55e227ebdd9ff507cbc074bed51c55da4984d5ed
j-zunino <zuninojuanbautista@gmail.com>
2025-03-12 03:15:29 -0300

    refactor(Thumbnail.tsx): improve readability and logic for icon selection
    - Added support for "image" and "nsfw".
    - Simplified conditional logic for determining the appropriate icon.
    - Ensured empty thumbnails default to the article icon.

--- src/reddit/Post/Thumbnail.tsx
+++ src/reddit/Post/Thumbnail.tsx
@@ -1,4 +1,10 @@
-import { IconArticle } from '@tabler/icons-react';
+import {
+    IconArticle,
+    IconPhoto,
+    IconVideo,
+    IconXxx
+} from '@tabler/icons-react';
+import { ReactNode } from 'react';
 
 interface Props {
     alt: string;
@@ -6,22 +12,38 @@ interface Props {
     is_video: boolean;
 }
 
+let icon: ReactNode;
+
 export const Thumbnail = ({ alt, thumbnail, is_video }: Props) => {
+    if (is_video) {
+        icon = <IconVideo />;
+    }
+
+    if (thumbnail === 'image') {
+        icon = <IconPhoto />;
+    }
+
+    if (thumbnail === 'nsfw') {
+        icon = <IconXxx />;
+    }
+
+    if (thumbnail === 'self' || thumbnail === 'default' || thumbnail === '') {
+        icon = <IconArticle />;
+    }
+
+    if (icon) {
+        return (
+            <div className="min-w-24 border-1 mr-4 hidden h-20 w-24 items-center justify-center rounded-md border-gray-200 bg-gray-100 text-gray-500 dark:border-zinc-800 dark:bg-black dark:text-zinc-700 lg:flex">
+                {icon}
+            </div>
+        );
+    }
+
     return (
-        <>
-            {is_video === true ||
-            thumbnail === 'self' ||
-            thumbnail === 'default' ? (
-                <div className="min-w-24 border-1 mr-4 hidden h-20 w-24 items-center justify-center rounded-md border-gray-200 bg-gray-100 text-gray-500 dark:border-zinc-800 dark:bg-black dark:text-zinc-700 lg:flex">
-                    <IconArticle />
-                </div>
-            ) : (
-                <img
-                    src={thumbnail}
-                    alt={alt}
-                    className="min-w-24 mr-4 hidden h-20 w-24 rounded-md object-cover lg:flex"
-                />
-            )}
-        </>
+        <img
+            src={thumbnail}
+            alt={alt}
+            className="min-w-24 mr-4 hidden h-20 w-24 rounded-md object-cover lg:flex"
+        />
     );
 };
