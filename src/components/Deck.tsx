d8a4ca34f10a0e7752d3eefac967ca6e36f8bc6a
j-zunino <zuninojuanbautista@gmail.com>
2025-03-22 15:25:30 -0300

    refactor(Deck): remove unnecessary fragment

--- src/components/Deck.tsx
+++ src/components/Deck.tsx
@@ -11,33 +11,28 @@ interface Props {
 
 export const Deck = ({ subRedditList }: Props) => {
     return (
-        <>
-            <div className="flex flex-1">
-                {subRedditList.length > 0 ? (
-                    <>
-                        {subRedditList.map((subReddit) => (
-                            <SubRedditContextProvider
-                                key={subReddit}
-                                subreddit={subReddit}
-                            >
-                                <SubReddit
-                                    key={subReddit}
-                                    subreddit={subReddit}
-                                />
-                            </SubRedditContextProvider>
-                        ))}
-                    </>
-                ) : (
-                    <div className="flex h-screen flex-1 flex-col items-center justify-center text-center text-sky-400">
-                        <IconArrowLeft className="left-15 absolute top-4" />
-                        <IconExclamationCircleFilled size={34} />
-                        <h1 className="text-2xl font-bold">
-                            There are no Subreddits to display!
-                        </h1>
-                        <p>Click the button on the sidebar to add one.</p>
-                    </div>
-                )}
-            </div>
-        </>
+        <div className="flex flex-1">
+            {subRedditList.length > 0 ? (
+                <>
+                    {subRedditList.map((subReddit) => (
+                        <SubRedditContextProvider
+                            key={subReddit}
+                            subreddit={subReddit}
+                        >
+                            <SubReddit key={subReddit} subreddit={subReddit} />
+                        </SubRedditContextProvider>
+                    ))}
+                </>
+            ) : (
+                <div className="flex h-screen flex-1 flex-col items-center justify-center text-center text-sky-400">
+                    <IconArrowLeft className="left-15 absolute top-4" />
+                    <IconExclamationCircleFilled size={34} />
+                    <h1 className="text-2xl font-bold">
+                        There are no Subreddits to display!
+                    </h1>
+                    <p>Click the button on the sidebar to add one.</p>
+                </div>
+            )}
+        </div>
     );
 };
