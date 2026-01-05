064b0c25fcae77cf9439077ada3e079dcd8e8b66
j-zunino <zuninojuanbautista@gmail.com>
2026-01-05 17:06:21 -0300

    feat: add state for subreddit input

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,49 @@
-import { useRef } from 'react';
+import { useRef, useState, type FormEvent } from 'react';
 import { Modal } from './components/shared/Modal';
 import { handleOpen } from './utils/modal.utils';
 
 function App() {
+    const [subreddit, setSubreddit] = useState('');
     const addSubredditRef = useRef<HTMLDialogElement>(null);
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        console.log(subreddit);
+    };
 
     return (
         <>
-            <div className="flex">
+            <div className="flex text-white">
                 <button
                     onClick={(e) => handleOpen(e, addSubredditRef)}
-                    className="bg-global-orange text-white hover:bg-global-orange-hover active:bg-global-orange-active"
+                    className="bg-global-orange hover:bg-global-orange-hover active:bg-global-orange-active"
                 >
                     Open
                 </button>
             </div>
 
             <Modal modalRef={addSubredditRef}>
-                <form className="flex max-w-100 flex-col gap-2 bg-global-bg-hover p-4">
-                    <input
-                        type="text"
-                        className="border border-global-border"
-                    />
-                    <button className="bg-global-orange text-white hover:bg-global-orange-hover active:bg-global-orange-active">
+                <form
+                    onSubmit={handleSubmit}
+                    className="flex max-w-100 flex-col gap-2 bg-global-bg-hover p-4"
+                >
+                    <label className="flex flex-col">
+                        Subreddit name:
+                        <input
+                            type="text"
+                            placeholder="reactjs"
+                            name="addSubredditInput"
+                            required={true}
+                            value={subreddit}
+                            onChange={(e) => setSubreddit(e.target.value)}
+                            className="border border-global-border outline-0 focus:border-global-orange"
+                        />
+                    </label>
+
+                    <button
+                        type="submit"
+                        className="bg-global-orange hover:bg-global-orange-hover active:bg-global-orange-active"
+                    >
                         Add
                     </button>
                 </form>
