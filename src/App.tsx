57049776f6cb0dfac7d0fd068e00e555151fdb0b
j-zunino <zuninojuanbautista@gmail.com>
2026-01-14 02:12:20 -0300

    feat: add mobile bottom bar

--- src/App.tsx
+++ src/App.tsx
@@ -16,14 +16,41 @@ function App() {
     return (
         <>
             <div className="flex">
-                <aside className="h-screen border-r p-2">
+                <aside className="hidden h-screen flex-col justify-between border-r p-2 md:flex">
                     <button
                         onClick={(e) => handleOpen(e, addSubredditRef)}
                         className="aspect-square px-2 hover:bg-surface-300"
                     >
                         +
                     </button>
+
+                    <a
+                        href="https://github.com/j-zunino/redditdeck"
+                        target="_blank"
+                        className="px-2 hover:bg-surface-300"
+                    >
+                        G
+                    </a>
                 </aside>
+
+                <div className="fixed bottom-0 mb-2 flex w-full justify-center md:hidden">
+                    <div className="flex justify-between border bg-surface-100 p-2">
+                        <button
+                            onClick={(e) => handleOpen(e, addSubredditRef)}
+                            className="px-4 py-2 hover:bg-surface-300"
+                        >
+                            +
+                        </button>
+
+                        <a
+                            href="https://github.com/j-zunino/redditdeck"
+                            target="_blank"
+                            className="px-4 py-2 hover:bg-surface-300"
+                        >
+                            GitHub
+                        </a>
+                    </div>
+                </div>
 
                 <div className="flex h-full max-h-screen overflow-x-auto overflow-y-hidden">
                     {subreddits.map((sub) => (
